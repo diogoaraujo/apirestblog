2183aeef6b19c528cec0a496cef4f47196b14dfd
diogoaraujo <araujo@SURFACE-A.localdomain>
2021-08-18 23:54:43 -0300

    Usando JEST para criar TDD

---
+++ server/test/posts.test.js
@@ -0,0 +1,16 @@
+const axios = require('axios');
+
+
+
+test('Should get posts', async function () {
+    const response = await axios ({
+        url: 'http://localhost:3000/posts',
+        method: 'get'
+    });
+    const posts = response.data;
+    expect(posts).toHaveLength(3);
+    const [firstPost] = posts;
+    expect(firstPost.ide).toBe(1);
+    expect(firstPost.title).toBe('REST API: Métodos');
+    
+});
